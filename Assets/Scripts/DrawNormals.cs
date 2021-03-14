cc120a47f9534fdd6de2b5fb312be4b4665eb534
Leinad-Dev <55466788+Leinad-Dev@users.noreply.github.com>
2021-03-13 18:07:12 -0800

    Initial commit

---
+++ Assets/Scripts/DrawNormals.cs
@@ -0,0 +1,83 @@
+﻿using UnityEngine;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+public class DrawNormals : MonoBehaviour
+{
+#if UNITY_EDITOR
+    [SerializeField]
+    private MeshFilter _meshFilter = null;
+    [SerializeField]
+    private NormalsDrawData _vertexNormals = new NormalsDrawData(new Color32(200, 255, 195, 127), false);
+
+    [System.Serializable]
+    private class NormalsDrawData
+    {
+        [SerializeField]
+        protected DrawType _draw = DrawType.Selected;
+        protected enum DrawType { Never, Selected, Always }
+        [SerializeField]
+        protected float _length = 0.3f;
+        [SerializeField]
+        protected Color _normalColor;
+        private Color _baseColor = new Color32(255, 133, 0, 255);
+        private const float _baseSize = 0.0125f;
+
+
+        public NormalsDrawData(Color normalColor, bool draw)
+        {
+            _normalColor = normalColor;
+            _draw = draw ? DrawType.Selected : DrawType.Never;
+        }
+
+        public bool CanDraw(bool isSelected)
+        {
+            return (_draw == DrawType.Always) || (_draw == DrawType.Selected && isSelected);
+        }
+
+        public void Draw(Vector3 from, Vector3 direction)
+        {
+            if (Camera.current.transform.InverseTransformDirection(direction).z < 0f)
+            {
+                Gizmos.color = _baseColor;
+                Gizmos.DrawWireSphere(from, _baseSize);
+
+                Gizmos.color = _normalColor;
+                Gizmos.DrawRay(from, direction * _length);
+            }
+        }
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        OnDrawNormals(true);
+    }
+
+    void OnDrawGizmos()
+    {
+        if (!Selection.Contains(this))
+            OnDrawNormals(false);
+    }
+
+    private void OnDrawNormals(bool isSelected)
+    {
+
+        Mesh mesh = _meshFilter.sharedMesh;
+
+        //Draw Vertex Normals
+        if (_vertexNormals.CanDraw(isSelected))
+        {
+            if (GetComponent<MeshFilter>().sharedMesh == null)
+                return;
+
+            Vector3[] vertices = mesh.vertices;
+            Vector3[] normals = mesh.normals;
+            for (int i = 0; i < vertices.Length; i++)
+            {
+                _vertexNormals.Draw(transform.TransformPoint(vertices[i]), transform.TransformVector(normals[i]));
+            }
+        }
+    }
+#endif
+}
---
+++ Assets/Scripts/procedural_geo.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class procedural_geo : MonoBehaviour
+{
+    private void Awake()
+    {
+        Mesh mesh = new Mesh();
+        mesh.name = "procQuad";
+
+        //step 1) create points
+        List<Vector3> points = new List<Vector3>()
+        {
+            new Vector3(-1, 1), //pt 0
+            new Vector3(1, 1), //pt 1
+            new Vector3(-1, -1), //pt 2
+            new Vector3(1, -1)  //pt 3
+        };
+
+        //step2) create triangle from points
+        int[] triIndices = new int[]{2,1,0,/*<--upper triangle*/ 2,3, 1 /*<--lower traignle*/};
+
+
+        mesh.SetVertices(points);
+        mesh.triangles = triIndices;
+
+        //step2) set normals
+        List<Vector3> normals = new List<Vector3>()
+        {
+            new Vector3(0,0,1),
+            new Vector3(0,0,1),
+            new Vector3(0,0,1),
+            new Vector3(0,0,1)
+        };
+        mesh.SetNormals(normals);
+
+
+
+
+        Debug.Log("triangle count = " + mesh.triangles.Length/3);
+        Debug.Log("vert count = " + mesh.vertexCount);
+
+        GetComponent<MeshFilter>().mesh = mesh;
+
+
+
+
+        //we need to make UVs now
+        //see video at 1:00:26
+
+
+    }
+
+}
